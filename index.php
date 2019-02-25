06e13620c49d4e64bc7a53155346b6bba78c8001
Lunga MAKWAKWA <lmakwakw@e4r10p4.wethinkcode.co.za>
2019-02-25 11:46:27 +0200

    final bot

--- index.php
+++ index.php
@@ -1,109 +1,368 @@
 <?php
 
-    $access_token = "EAAIDB7kFR8YBAKcSFIHGAemXVzAwmeyvy3vSen3WrNGoFdSiQWg88Ak766jPvuXPNtELMVSIGR1lsiDRN9BgZAkYv7quRxNzbgPCadJJtXlakrqMg9BwaIYsj6cdGiYrAs4nI8xGr7OvZBIZCbsrftlcZAJwkZBdvB2gZCMga7gwZDZD";
-    $verify_token = "MyCVBot";
-    $hub_verify_token = null;
+	include("Messenger.php");
+    $apiKey = "EAAIDB7kFR8YBAKcSFIHGAemXVzAwmeyvy3vSen3WrNGoFdSiQWg88Ak766jPvuXPNtELMVSIGR1lsiDRN9BgZAkYv7quRxNzbgPCadJJtXlakrqMg9BwaIYsj6cdGiYrAs4nI8xGr7OvZBIZCbsrftlcZAJwkZBdvB2gZCMga7gwZDZD";
+    
 
-    //var_dump($_REQUEST);
+	// Instances the Facebook class
+    $facebook = new Messenger($apiKey);
 
-        if(isset($_REQUEST['hub_challenge']))
-        {
-            $challenge = $_REQUEST['hub_challenge'];
-            $hub_verify_token = $_REQUEST['hub_verify_token'];
+    echo $_REQUEST['hub_challenge'];
+    
 
-            if ($hub_verify_token === $verify_token) 
-            {
-                echo $challenge;
-            }
-        }
-
-
-
-    //run this comand on terminal
-    //curl -X POST "https://graph.facebook.com/v3.2/me/subscribed_apps?access_token=EAAIDB7kFR8YBAKcSFIHGAemXVzAwmeyvy3vSen3WrNGoFdSiQWg88Ak766jPvuXPNtELMVSIGR1lsiDRN9BgZAkYv7quRxNzbgPCadJJtXlakrqMg9BwaIYsj6cdGiYrAs4nI8xGr7OvZBIZCbsrftlcZAJwkZBdvB2gZCMga7gwZDZD";
-
-    $input = json_decode(file_get_contents('php://input'), true);
-    $sender = $input['entry'][0]['messaging'][0]['sender']['id'];
-    $message = $input['entry'][0]['messaging'][0]['message']['text'];
+    // Take text and chat_id from the message
+    $text = $facebook->Text();
+    $msg = strtolower($text);
+	$chat_id = $facebook->ChatID();
+	//$message_id = $facebook->EntryID();
+	$message = "";
+    $result = "";
 
 
-    /////////////////////////////////////////////////////////////////////////////////////
-    //              MESSAGES TO SEND 
-    /////////////////////////////////////////////////////////////////////////////////////
+	if(!is_null($msg) && !is_null($chat_id))
+	{
+        //$facebook->sendChatAction($chat_id, 'typing_on');
+		if ((strpos($msg, 'hello') !== false) || (strpos($msg, 'hi') !== false) || (strpos($msg, 'goodmorning') !== false) || (strpos($msg, 'goodafternoon') !== false)) // simple text message
+		{
+			$facebook->sendChatAction($chat_id, 'mark_seen');
+            $facebook->sendChatAction($chat_id, 'typing_on');
+			$message = "Hello, How are you?";
+            $result = $facebook->sendMessage($chat_id, $message);
+            $facebook->sendChatAction($chat_id, 'typing_off');
+		}
 
-    $msg = strtolower($message);
+		else if ((strpos($msg, 'good') !== false) || (strpos($msg, 'great') !== false) || (strpos($msg, 'fine') !== false) || (strpos($msg, 'ok') !== false)) // quick replies
+		{
+			$facebook->sendChatAction($chat_id, 'mark_seen');
+			$facebook->sendChatAction($chat_id, 'typing_on');
+			$message = "I am good thanks, Here are some questions you can ask me?";
+			$replies = array(
+				array(
+					'content_type' => 'text',
+					'title' => 'Whats your Name?',
+					'payload' => 'PAYLOAD_ONE'
+				),
+				array(
+					'content_type' => 'text',
+					'title' => 'qualifications?',
+					'payload' => 'PAYLOAD_TWO'
+				),
+				array(
+					'content_type' => 'text',
+					'title' => 'Where do you live?',
+					'payload' => 'PAYLOAD_THREE'
+				)
+			);
+			$result = $facebook->sendQuickReply($chat_id, $message, $replies);
+			$facebook->sendChatAction($chat_id, 'typing_off');
+		}
 
-    if ((strpos($msg, 'hello') !== false) || (strpos($msg, 'hi') !== false) || (strpos($msg, 'goodmorning') !== false) || (strpos($msg, 'goodafternoon') !== false))
-    {
-        $message_to_reply = "Hello Welcome to my CV";
-    }
+		else if ((strpos($msg, 'name') !== false) || (strpos($msg, 'old') !== false)) // quick replies
+		{
+			$facebook->sendChatAction($chat_id, 'mark_seen');
+			$facebook->sendChatAction($chat_id, 'typing_on');
+			$message = "My name is Lunga Makwakwa and i am 21 years old.";
+			$replies = array(
+				array(
+					'content_type' => 'text',
+					'title' => 'about yourself?',
+					'payload' => 'PAYLOAD_ONE'
+				),
+				array(
+					'content_type' => 'text',
+					'title' => 'Where do you live?',
+					'payload' => 'PAYLOAD_TWO'
+				),
+				array(
+					'content_type' => 'text',
+					'title' => 'qualifications?',
+					'payload' => 'PAYLOAD_THREE'
+				)
+			);
+			$result = $facebook->sendQuickReply($chat_id, $message, $replies);
+			$facebook->sendChatAction($chat_id, 'typing_off');
+		}
 
-    else if ((strpos($msg, 'name') !== false))
-    {
-        $message_to_reply = "My name is Lunga Makwakwa";
-    }
+		else if ((strpos($msg, 'from') !== false) || (strpos($msg, 'live') !== false)) // quick replies
+		{
+			$facebook->sendChatAction($chat_id, 'mark_seen');
+			$facebook->sendChatAction($chat_id, 'typing_on');
+			$message = "Commissioner St & Rissik St, Johannesburg, 2001";
+			$replies = array(
+				array(
+					'content_type' => 'text',
+					'title' => 'qualifications?',
+					'payload' => 'PAYLOAD_ONE'
+				),
+				array(
+					'content_type' => 'text',
+					'title' => 'about yourself?',
+					'payload' => 'PAYLOAD_TWO'
+				),
+				array(
+					'content_type' => 'text',
+					'title' => 'languages?',
+					'payload' => 'PAYLOAD_THREE'
+				)
+			);
+			$result = $facebook->sendQuickReply($chat_id, $message, $replies);
+			$facebook->sendChatAction($chat_id, 'typing_off');
+		}
 
-    else if ((strpos($msg, 'github') !== false) || (strpos($msg, 'repository') !== false))
-    {
-        $message_to_reply = "My github link is https://github.com/The-only-blue";
-    }
+		else if ((strpos($msg, 'about yourself') !== false)) // quick replies
+		{
+			$facebook->sendChatAction($chat_id, 'mark_seen');
+			$facebook->sendChatAction($chat_id, 'typing_on');
+			$message = "I am a very strong, reliable and ambitious person. I strive and thrive on new challenges and intend to see challenges that bring posi3ve result in all aspects. I adapt well to new situa3ons, follow instruc3ons accurately and also develop confidence to handle day to day problems appropriately.";
+			$replies = array(
+				array(
+					'content_type' => 'text',
+					'title' => 'languages?',
+					'payload' => 'PAYLOAD_ONE'
+				),
+				array(
+					'content_type' => 'text',
+					'title' => 'qualifications',
+					'payload' => 'PAYLOAD_TWO'
+				),
+				array(
+					'content_type' => 'text',
+					'title' => 'intrests or hobbies?',
+					'payload' => 'PAYLOAD_THREE'
+				)
+			);
+			$result = $facebook->sendQuickReply($chat_id, $message, $replies);
+			$facebook->sendChatAction($chat_id, 'typing_off');
+		}
 
-    else if ((strpos($msg, 'from') !== false) || (strpos($msg, 'live') !== false))
-    {
-        $message_to_reply = "Commissioner St & Rissik St, Johannesburg, 2001";
-    }
+		else if ((strpos($msg, 'study') !== false) || (strpos($msg, 'education') !== false) || (strpos($msg, 'qualifications') !== false)) // quick replies
+		{
+			$facebook->sendChatAction($chat_id, 'mark_seen');
+			$facebook->sendChatAction($chat_id, 'typing_on');
+			$message = "MICT SETA NQF LEVEL 5 (May 2018 – Present)".chr(10)."WeThinkCode_,".chr(10)."".chr(10)."Tech Support NQF Level 4 (Jan 2017 – Dec 2017)".chr(10)."ICollege,".chr(10)."".chr(10)."National Senior Certificate (Jan 2010 – Dec 2015),".chr(10)."Edenglen High School";
+			$replies = array(
+				array(
+					'content_type' => 'text',
+					'title' => 'intrests or hobbies?',
+					'payload' => 'PAYLOAD_ONE'
+				),
+				array(
+					'content_type' => 'text',
+					'title' => 'languages?',
+					'payload' => 'PAYLOAD_TWO'
+				),
+				array(
+					'content_type' => 'text',
+					'title' => 'github repo?',
+					'payload' => 'PAYLOAD_THREE'
+				)
+			);
+			$result = $facebook->sendQuickReply($chat_id, $message, $replies);
+			$facebook->sendChatAction($chat_id, 'typing_off');
+		}
 
-    else if ((strpos($msg, 'about yourself') !== false) || (strpos($msg, 'school') !== false))
-    {
-        $message_to_reply = "I am a very strong, reliable and ambi3ous person. I strive and thrive on new challenges and intend to see challenges that bring posi3ve result in all aspects. I adapt well to new situa3ons, follow instruc3ons accurately and also develop confidence to handle day to day problems appropriately.";
-    }
+		else if ((strpos($msg, 'languages') !== false)) // quick replies
+		{
+			$facebook->sendChatAction($chat_id, 'mark_seen');
+			$facebook->sendChatAction($chat_id, 'typing_on');
+			$message = "PHP".chr(10)."Node.js".chr(10)."C".chr(10)."JS".chr(10)."SQL".chr(10)."C++".chr(10)."HTML/5".chr(10)."CSS/3".chr(10)."bootstrap".chr(10)."UNIX/POSIX".chr(10)."willing to learn more";
+			$replies = array(
+				array(
+					'content_type' => 'text',
+					'title' => 'intrests or hobbies?',
+					'payload' => 'PAYLOAD_ONE'
+				),
+				array(
+					'content_type' => 'text',
+					'title' => 'github repo?',
+					'payload' => 'PAYLOAD_TWO'
+				),
+				array(
+					'content_type' => 'text',
+					'title' => 'Where do you live?',
+					'payload' => 'PAYLOAD_THREE'
+				)
+			);
+			$result = $facebook->sendQuickReply($chat_id, $message, $replies);
+			$facebook->sendChatAction($chat_id, 'typing_off');
+		}
 
-    else if ((strpos($msg, 'study') !== false) || (strpos($msg, 'education') !== false) || (strpos($msg, 'qualification') !== false))
-    {
-        $message_to_reply = "MICT SETA NQF LEVEL 5 (May 2018 – Present) WeThinkCode_, Tech Support NQF Level 4 (Jan 2017 – Dec 2017) ICollege, National Senior Certificate (Jan 2010 – Dec 2015), Edenglen High School";
-    }
+		else if ((strpos($msg, 'intrests') !== false) || (strpos($msg, 'hobby') !== false)) // quick replies
+		{
+			$facebook->sendChatAction($chat_id, 'mark_seen');
+			$facebook->sendChatAction($chat_id, 'typing_on');
+			$message = "Gaming: Conceptual gammer.".chr(10)."Graphic Design: Logo and Album Art design.".chr(10)."Music Production: I produces, write and mix music.".chr(10)."Tech news: Reading up on the latest Tech.";
+			$replies = array(
+				array(
+					'content_type' => 'text',
+					'title' => 'languages?',
+					'payload' => 'PAYLOAD_ONE'
+				),
+				array(
+					'content_type' => 'text',
+					'title' => 'Whats your name?',
+					'payload' => 'PAYLOAD_TWO'
+				),
+				array(
+					'content_type' => 'text',
+					'title' => 'Where do you live?',
+					'payload' => 'PAYLOAD_THREE'
+				)
+			);
+			$result = $facebook->sendQuickReply($chat_id, $message, $replies);
+			$facebook->sendChatAction($chat_id, 'typing_off');
+		}
 
-    else if ((strpos($msg, 'languages') !== false))
-    {
-        $message_to_reply = "PHP, Node.js, C, JS, SQL, C++, HTML/5, CSS/3, bootstrap, UNIX/POSIX and willing to learn more";
-    }
+		// else if ((strpos($msg, 'github') !== false) || (strpos($msg, 'repository') !== false)) // buttons
+		// {
+		// 	$message = "My github link is:";
+		// 	$button = array(
+		// 		array(
+		// 			'type' => 'web_url',
+		// 			'url' => 'https://github.com/The-only-blue',
+		// 			'title' => 'Github Link'
+		// 		),
+		// 		array(
+		// 			'type' => 'web_url',
+		// 			'url' => 'https://google.com',
+		// 			'title' => 'Button 2'
+		// 		),
+		// 		array(
+		// 			'type' => 'web_url',
+		// 			'url' => 'https://google.com',
+		// 			'title' => 'Button 3'
+		// 		)
+		// 	);
+		// 	$result = $facebook->sendButtonTemplate($chat_id, $message, $button);
+		// }
+		else if ($text == "replies") // quick replies
+		{
+			$message = "Pick one";
+			$replies = array(
+				array(
+					'content_type' => 'text',
+					'title' => 'Option One',
+					'payload' => 'PAYLOAD_ONE'
+				),
+				array(
+					'content_type' => 'text',
+					'title' => 'Option Two',
+					'payload' => 'PAYLOAD_TWO'
+				),
+				array(
+					'content_type' => 'text',
+					'title' => 'Option Three',
+					'payload' => 'PAYLOAD_THREE'
+				)
+			);
+			$result = $facebook->sendQuickReply($chat_id, $message, $replies);
+		}
+		else if ((strpos($msg, 'github') !== false) || (strpos($msg, 'repository') !== false)) // generic template
+		{
+			$facebook->sendChatAction($chat_id, 'mark_seen');
+			$facebook->sendChatAction($chat_id, 'typing_on');
+			$button = array(
+				array(
+					'type' => 'web_url',
+					'url' => 'https://github.com/The-only-blue',
+					'title' => 'Github: The-only-blue.'
+				),
+				array(
+					
+					"type" => "postback",
+  					"title" => "Coding languages?",
+  					"payload" => 'PAYLOAD_ONE'
+				)
+			);
+			$elements = array(
+				array(
+					'title' => 'Github',
+					'item_url' => 'https://github.com/The-only-blue',
+					'image_url' => 'https://www.uruit.com/blog/wp-content/uploads/2017/08/github.png',
+					'subtitle' => 'Life of code.',
+					'buttons' => $button
+				));
+			$result = $facebook->sendGenericTemplate($chat_id, $elements);
+			$facebook->sendChatAction($chat_id, 'typing_off');
+		}
+		
+		// else if ((strpos($msg, 'github') !== false) || (strpos($msg, 'repository') !== false)) // generic template
+		// {
+		// 	$button = array(
+		// 		array(
+		// 			'type' => 'web_url',
+		// 			'url' => 'https://github.com/The-only-blue',
+		// 			'title' => 'Github: The-only-blue.'
+		// 		),
+		// 		array(
+		// 			'type' => 'web_url',
+		// 			'url' => 'https://google.com',
+		// 			'title' => 'Button Two'
+		// 		)
+		// 	);
+		// 	$elements = array(
+		// 		array(
+		// 			'title' => 'Github',
+		// 			'item_url' => 'https://github.com/The-only-blue',
+		// 			'image_url' => 'https://www.uruit.com/blog/wp-content/uploads/2017/08/github.png',
+		// 			'subtitle' => 'Life of code.',
+		// 			'buttons' => $button
+		// 		),
+		// 		array(
+		// 			'title' => 'Title Two',
+		// 			'item_url' => 'https://google.com',
+		// 			'image_url' => 'http://placehold.it/350x350',
+		// 			'subtitle' => 'Item Description Here',
+		// 			'buttons' => $button
+		// 		),
+		// 		array(
+		// 			'title' => 'Title Three',
+		// 			'item_url' => 'https://google.com',
+		// 			'image_url' => 'http://placehold.it/350x350',
+		// 			'subtitle' => 'Item Description Here',
+		// 			'buttons' => $button
+		// 		),
+		// 		array(
+		// 			'title' => 'Title Four',
+		// 			'item_url' => 'https://google.com',
+		// 			'image_url' => 'http://placehold.it/350x350',
+		// 			'subtitle' => 'Item Description Here',
+		// 			'buttons' => $button
+		// 		),
+		// 		array(
+		// 			'title' => 'Title Five',
+		// 			'item_url' => 'https://google.com',
+		// 			'image_url' => 'http://placehold.it/350x350',
+		// 			'subtitle' => 'Item Description Here',
+		// 			'buttons' => $button
+		// 		)
+		// 	);
+		// 	$result = $facebook->sendGenericTemplate($chat_id, $elements);
+        // }
+        // else
+        // {
+        //     $facebook->sendChatAction($chat_id, 'typing_on');
+        //     $message = "Here are some questions you can ask me.";
+		// 	$replies = array(
+		// 		array(
+		// 			'content_type' => 'text',
+		// 			'title' => 'How old are you?',
+		// 			'payload' => 'PAYLOAD_ONE'
+		// 		),
+		// 		array(
+		// 			'content_type' => 'text',
+		// 			'title' => 'Where do you live?',
+		// 			'payload' => 'PAYLOAD_TWO'
+		// 		),
+		// 		array(
+		// 			'content_type' => 'text',
+		// 			'title' => 'OpWhat are your qualifications?',
+		// 			'payload' => 'PAYLOAD_THREE'
+		// 		)
+		// 	);
+        //     $result = $facebook->sendQuickReply($chat_id, $message, $replies);
+        //     $facebook->sendChatAction($chat_id, 'typing_off');
 
-    else if ((strpos($msg, 'intrests') !== false) || (strpos($msg, 'hobby') !== false))
-    {
-        $message_to_reply = "Gaming: Conceptual gammer. Graphic Design: Logo and Album Art design. Music Production: I produces, write and mix music. Tech news: Reading up on the latest Tech.";
-    }
-
-    else
-    {
-        $message_to_reply = "huh";
-    }
-
-    //API Url
-    $url = 'https://graph.facebook.com/v3.2/me/messages?access_token='.$access_token;
-
-    //Initiate cURL.
-    $ch = curl_init($url);
-
-    //The JSON data.
-    $jsonData = '{"recipient":{"id":"'.$sender.'"},"message":{"text":"'.$message_to_reply.'"}}';
-
-    //Encode the array into JSON.
-    $jsonDataEncoded = $jsonData;
-
-    //Tell cURL that we want to send a POST request.
-    curl_setopt($ch, CURLOPT_POST, 1);
-
-    //Attach our encoded JSON string to the POST fields.
-    curl_setopt($ch, CURLOPT_POSTFIELDS, $jsonDataEncoded);
-
-    //Set the content type to application/json
-    curl_setopt($ch, CURLOPT_HTTPHEADER, array('Content-Type: application/json'));
-
-    //curl_setopt($ch, CURLOPT_HTTPHEADER, array('Content-Type: application/x-www-form-urlencoded'));
-
-    //Execute the request
-    if(!empty($input['entry'][0]['messaging'][0]['message'])){
-        $result = curl_exec($ch);
-    }
-
+        // }
+	}
 ?>
